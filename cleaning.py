f14f7c2778281339906bb023523b16f828251bb9
cgreen14 <cgreen14@nd.edu>
2019-02-20 15:40:04 -0500

    cleaning python

---
+++ cleaning.py
@@ -0,0 +1,13 @@
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt # plotting
+import numpy as np # linear algebra
+import os # accessing directory structure
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+nRowsRead = 1000 # specify 'None' if want to read whole file
+# globalterrorismdb.csv has 170350 rows in reality, but we are only loading/previewing the first 1000 rows
+df1 = pd.read_csv('../ProjData/globalterrorismdb_0718.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'globalterrorismdb.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
