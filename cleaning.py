1eddb4c21322781fdb3debc9246ef246ce697164
cgreen14 <cgreen14@nd.edu>
2019-02-20 15:54:30 -0500

    data labels

--- cleaning.py
+++ cleaning.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.6
+
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt # plotting
@@ -7,7 +9,10 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 nRowsRead = 1000 # specify 'None' if want to read whole file
 # globalterrorismdb.csv has 170350 rows in reality, but we are only loading/previewing the first 1000 rows
-df1 = pd.read_csv('../ProjData/globalterrorismdb_0718.csv', delimiter=',', nrows = nRowsRead)
-df1.dataframeName = 'globalterrorismdb.csv'
-nRow, nCol = df1.shape
+dataSet = pd.read_csv('../ProjData/globalterrorismdb_0718dist.csv', delimiter=',', encoding="ISO-8859-1")
+dataSet.dataframeName = 'globalterrorismdb.csv'
+nRow, nCol = dataSet.shape
 print(f'There are {nRow} rows and {nCol} columns')
+
+dataSet.rename(columns={'iyear':'Year','imonth':'Month','iday':'Day','country_txt':'Country','region_txt':'Region','attacktype1_txt':'AttackType','target1':'Target','nkill':'Killed','nwound':'Wounded','summary':'Summary','gname':'Group','targtype1_txt':'Target_type','weaptype1_txt':'Weapon_type','motive':'Motive'},inplace=True)
+dataSet=dataSet[['Year','Month','Day','Country','Region','city','latitude','longitude','AttackType','Killed','Wounded','Target','Summary','Group','Target_type','Weapon_type','Motive']]
