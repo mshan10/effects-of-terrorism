5cec8d9643513d3edc257bc8e465528ab5e527d9
Rich <zhengruiqi@rich.dhcp.nd.edu>
2019-02-20 16:15:02 -0500

    running now

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.6
+
 
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.preprocessing import StandardScaler
@@ -12,7 +12,7 @@ nRowsRead = 1000 # specify 'None' if want to read whole file
 dataSet = pd.read_csv('../ProjData/globalterrorismdb_0718dist.csv', delimiter=',', encoding="ISO-8859-1")
 dataSet.dataframeName = 'globalterrorismdb.csv'
 nRow, nCol = dataSet.shape
-print(f'There are {nRow} rows and {nCol} columns')
+print("There are " + str(nRow) + " rows in the dataset")
 
 dataSet.rename(columns={'iyear':'Year','imonth':'Month','iday':'Day','country_txt':'Country','region_txt':'Region','attacktype1_txt':'AttackType','target1':'Target','nkill':'Killed','nwound':'Wounded','summary':'Summary','gname':'Group','targtype1_txt':'Target_type','weaptype1_txt':'Weapon_type','motive':'Motive'},inplace=True)
 dataSet=dataSet[['Year','Month','Day','Country','Region','city','latitude','longitude','AttackType','Killed','Wounded','Target','Summary','Group','Target_type','Weapon_type','Motive']]
